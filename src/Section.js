1a9888d33554298050766e3c3fd2d1d7f33f7bb6
Ayush Verma <itsmedoraemon19@gmail.com>
2024-02-23 23:06:19 +0530

    started

---
+++ src/Section.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Section.css";
+const Section = () => {
+  return (
+    <div className="main m-10">
+      <h1 className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent  bg-clip-text text-7xl letter-spacing: -0.05em; font-bold">
+        Our IITian Team
+      </h1>
+      <div className="">
+        <p className="text-white m-20 font-medium text-lg">A Team Of IITians to Boost your Tech Career to New Heights. The Curriculum Team is an experienced group of data analyst instructors</p>
+      </div>
+    </div>
+  );
+};
+
+export default Section;
